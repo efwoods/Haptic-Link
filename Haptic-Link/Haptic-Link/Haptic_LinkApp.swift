79ec7f5619a5d12a635f9501b76d463816bf1540
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-03-27 20:30:35 -0400

    init iPhone app:

---
+++ Haptic-Link/Haptic-Link/Haptic_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Haptic_LinkApp.swift
+//  Haptic-Link
+//
+//  Created by Evan Woods on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Haptic_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
